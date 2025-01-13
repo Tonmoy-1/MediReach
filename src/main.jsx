30dbdda2af25d9ea620ea89944c2be4c888fcdbc
Tonmoy <tonmoyhosen767@gmail.com>
2025-01-13 21:24:47 +0600

    setting basic route

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+// import AuthProvider from "./providers/AuthProvider";
+import { router } from "./Routes/router";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+    {/* <AuthProvider></AuthProvider> */}
+  </StrictMode>
+);
